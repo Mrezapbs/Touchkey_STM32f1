bd06ee8238f4992f0fd906951cf2bf6bd5ed4bda
Mrezapbs <125213272+Mrezapbs@users.noreply.github.com>
2024-05-02 22:04:54 +0330

    Update Touchkeys.h

--- Touchkeys.h
+++ Touchkeys.h
@@ -21,5 +21,80 @@ typedef enum
 
 void Keys_Handler(void);
 
+typedef union
+{
+	struct
+	{
+		struct
+		{
+			uint8_t OperationType;
+			uint8_t SwingMode;
+			uint8_t NumberOfSlaves;
+			uint8_t BGcolorSelect;
+			uint8_t OffTimerMinute;
+			uint8_t OffTimerHour;
+//			uint8_t TouckeySound;
+		}Setting;
+		struct
+		{
+			uint8_t PowerStatus;
+			uint8_t OperationModeStatus;
+			uint8_t FanVelocityOperation;
+			uint8_t SetTemperature;
+
+			int16_t RoomTemp;
+			uint8_t OutdoorAirTemp;
+			uint8_t IndoorCoilTemp;
+			uint8_t DefrostStatus;
+			uint8_t ModuleLockStatus;
+			uint8_t TimerCounter;
+		}Data;
+	}Struct;
+	uint16_t ErrorBuff[16];
+	uint8_t Buff[11];
+
+}OperatingStatusType;
+
+typedef struct
+{
+	struct
+	{
+		uint8_t TouchKeysStatus;
+		uint8_t TouchedKey;
+		uint8_t	TouchKeyCounter;
+		uint8_t IdleKeysCounter;
+		uint8_t Initializer;
+	}Touchkeys;
+	struct
+	{
+		uint16_t ScreenMenu;
+		uint8_t  WaitScreenFlag;
+		uint8_t  ErrorByte;
+		uint8_t  ErrorByteAddress;
+		uint8_t  ErrorFound;
+		uint8_t  Initializer;
+		uint8_t	 Execution;
+		uint16_t BackgroundColor;
+	}Display;
+	struct
+	{
+		uint16_t ADCRead[10];
+		uint16_t ADCReadSum;
+		uint16_t ADCReadAvg;
+	}Analog;
+	struct
+	{
+		uint8_t Status;
+		uint8_t Second;
+	}OffTimer;
+	uint8_t StartUpDelay;
+	uint8_t EEPROMStatus;
+	uint8_t EEPROMBuff[10];
+}FunctionalStatusType;
+
+
+extern OperatingStatusType   UserDefinedStatus;
+extern OperatingStatusType   ScreenStatus;
+extern FunctionalStatusType  FunctionalStatus;
 
 #endif
